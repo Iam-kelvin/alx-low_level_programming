d4611a4d56e4b63f092dea2229eea26e7ba5ef4b
Kelvin <kelvinajala007@gmail.com>
2022-09-15 16:33:39 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * main - test function that prints if integer
- *	is positive or negative
- * 	when given case 0
+ * is positive or negative
+ * when given case 0
  * Return: 0 (Successful)
  */
 

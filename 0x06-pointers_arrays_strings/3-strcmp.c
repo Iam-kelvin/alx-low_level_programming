4e35a61875620f424484718e6eefac7c9525ef17
Kelvin <kelvinajala007@gmail.com>
2022-09-22 15:07:20 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: -ve integer, 0 or +ve integer
+ * if s1 is less than, equal to or
+ * greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int diff = 0;
+	while (diff == 0)
+	{
+	diff = *s1 - *s2;
+	/* break if one of the strings is empty */
+	if (*s1 == '\0' || *s2 == '\0')
+	break;
+	s1++;
+	s2++;
+	}
+	return (diff);
+}

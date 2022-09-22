b21e804e1c045ef58181866f4b4644883a0a9c93
Kelvin <kelvinajala007@gmail.com>
2022-09-22 15:49:58 -0700

    Advanced_2

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ */
+void print_number(int n)
+{
+	unsigned int p = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		p = -p;
+	}
+	if (p / 10)
+	{
+		print_number(p / 10);
+	}
+	_putchar(p % 10 + '0');
+}
